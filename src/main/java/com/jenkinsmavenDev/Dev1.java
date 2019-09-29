db8d3871ae6c92cf42573b44353ab10205567909
Deepak Gill <deepak.gi@HCL.COM>
2019-09-29 23:42:15 +0530

    jenkins maven project

---
+++ src/main/java/com/jenkinsmavenDev/Dev1.java
@@ -0,0 +1,15 @@
+package com.jenkinsmavenDev;
+
+class Dev1{
+	public static void main(String args[]) {
+		
+		Dev1 dev = new Dev1();
+		dev.display();
+		
+	}
+	
+	public void display() {
+		System.out.println("Deepak GIll in Dev");
+	}
+	
+}
---
+++ src/test/java/com/jenkinsmavenTest/Dev1Test.java
@@ -0,0 +1,13 @@
+package com.jenkinsmavenTest;
+
+import org.testng.annotations.Test;
+
+public class Dev1Test {
+
+	
+	@Test
+	public void test() {
+		System.out.println("I am in test 1");
+	}
+
+}
